ee85121f95eed27f17727853cc2f1ddccc97015f
Elemen.Z <element.zhan@gmail.com>
2018-10-27 14:45:06 +0800

    add some initial data for testing , everything seem to be ok , done for my work , cheers ~

--- website/config/bootstrap.js
+++ website/config/bootstrap.js
@@ -9,8 +9,62 @@
  * https://sailsjs.com/config/bootstrap
  */
 
-module.exports.bootstrap = async function(done) {
+module.exports.bootstrap = async function (done) {
 
+  if (await Activity.count() > 0) {
+    return done();
+  }
+
+  await Activity.createEach([{
+      name: "reading activity",
+      time: "19",
+      img_url: "http://big5.minghui.org/mh/article_images/2014-9-9-minghui-newyork-1--ss.jpg",
+      short_description: "These activities have been developed for teachers to use as a guide",
+      full_description: "These activities have been developed for teachers to use as a guide. Our intent is to give teachers a framework to follow for use in a Holocaust unit of study. The philosophy behind these activities is based on constructivist pedagogy, brain-based learning and the awareness of the theory of multiple intelligences. We hope teachers will use them and build on them to fit the needs of students, taking into account their age, maturity, and ability levels.",
+      event_date: "2018-9-30",
+      organizer: "student",
+      venue: "LT1",
+      quota: "30",
+      high_light: "high_light",
+    },
+    {
+      name: "reading activity",
+      time: "19",
+      img_url: "http://big5.minghui.org/mh/article_images/2014-9-9-minghui-newyork-1--ss.jpg",
+      short_description: "These activities have been developed for teachers to use as a guide",
+      full_description: "These activities have been developed for teachers to use as a guide. Our intent is to give teachers a framework to follow for use in a Holocaust unit of study. The philosophy behind these activities is based on constructivist pedagogy, brain-based learning and the awareness of the theory of multiple intelligences. We hope teachers will use them and build on them to fit the needs of students, taking into account their age, maturity, and ability levels.",
+      event_date: "2018-9-30",
+      organizer: "student",
+      venue: "LT1",
+      quota: "30",
+      high_light: "high_light",
+    },
+    {
+      name: "reading activity",
+      time: "19",
+      img_url: "http://big5.minghui.org/mh/article_images/2014-9-9-minghui-newyork-1--ss.jpg",
+      short_description: "These activities have been developed for teachers to use as a guide",
+      full_description: "These activities have been developed for teachers to use as a guide. Our intent is to give teachers a framework to follow for use in a Holocaust unit of study. The philosophy behind these activities is based on constructivist pedagogy, brain-based learning and the awareness of the theory of multiple intelligences. We hope teachers will use them and build on them to fit the needs of students, taking into account their age, maturity, and ability levels.",
+      event_date: "2018-9-30",
+      organizer: "student",
+      venue: "LT1",
+      quota: "30",
+      high_light: "high_light",
+    },
+    {
+      name: "reading activity",
+      time: "19",
+      img_url: "http://big5.minghui.org/mh/article_images/2014-9-9-minghui-newyork-1--ss.jpg",
+      short_description: "These activities have been developed for teachers to use as a guide",
+      full_description: "These activities have been developed for teachers to use as a guide. Our intent is to give teachers a framework to follow for use in a Holocaust unit of study. The philosophy behind these activities is based on constructivist pedagogy, brain-based learning and the awareness of the theory of multiple intelligences. We hope teachers will use them and build on them to fit the needs of students, taking into account their age, maturity, and ability levels.",
+      event_date: "2018-9-30",
+      organizer: "student",
+      venue: "LT1",
+      quota: "30",
+      high_light: "high_light",
+    },
+    // etc.
+  ]);
   // By convention, this is a good place to set up fake data during development.
   //
   // For example:
